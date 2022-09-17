fcc96295940bcec76d3880a9fa17966a909f7664
Swapnil <swapnilandhale3@gmail.com>
2022-09-17 21:16:02 +0530

    Array

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,19 @@
+// An array of "String"
+
+let student = ["Ajhar", "Sanket", "Ravi"]
+
+// Array of "Int"
+
+var rollNumber = [15, 20, 13, 1, 65]
+//order  - index start from 0
+rollNumber[0]
+rollNumber[1]
+rollNumber[2]
+// rollNumber[5] - error
+
+//value add / Insertion
+
+rollNumber.count // count starts from 1
+rollNumber.append(30)
+rollNumber.count
+rollNumber.insert(50, at: 0) // at what index
